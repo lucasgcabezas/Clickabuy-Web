ca5212477b96080a26363b17a1a99179fe6921d8
lucasgcbzs <77704551+lucasgcbzs@users.noreply.github.com>
2021-05-27 18:10:19 -0300

    Jona no avisa

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api', router)
-mongoose.set('returnOriginal', false);
 
 app.listen(4000, () => console.log("App listening on port 4000"))
 // if (process.env.NODE_ENV === "production") {
