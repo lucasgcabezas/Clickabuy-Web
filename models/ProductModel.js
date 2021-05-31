81c33cfbdd56da74b94ec5ab32971ba3ce5e665a
diana sofia parra <sofiaparra.15@gmail.com>
2021-05-31 04:58:08 -0300

    commit sofia 31/5 0500

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -3,7 +3,7 @@ const mongoose = require ('mongoose');
 const productSchema = new mongoose.Schema({
     nameProduct:{type: String, required: true},
     productImg: {type: String, required: true},
-    description:{type: String, required: true},
+    description:{type: String},
     price:{type: Number, required: true},
     storeId:{type: mongoose.Types.ObjectId ,ref:'store'},
     stock: {type: Number ,default: 0}    
