df2de143735e23677ab9fc54fb017018f536ffd4
lucasgcbzs <77704551+lucasgcbzs@users.noreply.github.com>
2021-05-31 09:58:36 -0300

    mergeo grupal 31-05 1000

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,13 +5,8 @@ import { connect } from "react-redux";
 import authActions from "../redux/actions/authActions";
 import "../../src/gracia.css";
 const Header = (props) => {
-<<<<<<< HEAD
-  /* console.log(props.userLogged.userImg); */
-  /* console.log(props.userLogged.loggedWithGoogle); */
-=======
   const usuarioImage = props.userLogged ? <div style={{backgroundImage: "url('./assets" + props.userLogged.userImg + "')"}} className="usuarioImage"></div> : <span className="material-icons-outlined iconUser">account_circle</span>
   
->>>>>>> parra
   return (
     <header className="headerContainer">
       <div className="contenedorLogo">
@@ -29,40 +24,6 @@ const Header = (props) => {
         <span className="material-icons-outlined iconSearchHome">search</span>
       </div>
       <div className="contenedorNavs">
-<<<<<<< HEAD
-        <div
-          className="clickabuyLogo userImg"
-          style={
-            props.userLogged
-              ? props.userLogged.loggedWithGoogle
-                ? {
-                    backgroundImage: "url('" + props.userLogged.userImg + "')",
-
-                    /* backgroundImage: "url('./assets" + props.userLogged.userImg + "')", */
-                  }
-                : { backgroundImage: "url('./assets" + props.userLogged.userImg + "')" }
-              : {
-                  backgroundImage: "url('https://imagizer.imageshack.com/img923/3460/7gImHW.png')",
-                }
-          }
-        >
-          {" "}
-        </div>
-        <NavLink exact to="/" className="navegadores">
-          Home
-        </NavLink>
-        <Categories />
-        <NavLink to="/buys" className="navegadores">
-          <span className="material-icons-outlined iconCart">shopping_cart</span>
-        </NavLink>
-        {/* <Link to="/" className="navegadores"><span >Log out</span></Link> */}
-        {/* onClick={() => props.signOut()} */}
-        {/* </>} */}
-        {/* {!props.userLogged && <> */}
-        <NavLink to="/signUp" className="navegadores">
-          <span className="material-icons-outlined iconUser">account_circle</span>
-        </NavLink>
-=======
         <NavLink exact to="/" className="navegadores">Home</NavLink>
         <Categories />
         <NavLink to="/buys" className="navegadores"><span className="material-icons-outlined iconCart">shopping_cart</span></NavLink>
@@ -71,7 +32,6 @@ const Header = (props) => {
         {/* </>} */}
         {/* {!props.userLogged && <> */}
         <NavLink to="/signUp" className="navegadores">{usuarioImage}</NavLink>
->>>>>>> parra
         {/* </>} */}
       </div>
     </header>
