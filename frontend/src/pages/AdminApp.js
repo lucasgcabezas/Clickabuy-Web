7df1b16f8ec70461983ed236609444cc67a4b3b6
Jonathan Huata Vasquez <jonathan_huata@outlook.com>
2021-06-03 01:30:34 -0300

    se maqueto el panel de adminApp

---
+++ frontend/src/pages/AdminApp.js
@@ -0,0 +1,77 @@
+import adminAppActions from '../redux/actions/adminAppActions'
+import {connect} from 'react-redux'
+import {useState,useEffect} from 'react'
+
+
+
+
+
+const AdminApp = ({userLogged,getAllRequest,approveRequest,rejectRequest}) => {
+    const [requests,setRequests] = useState(null);
+
+    /*const fetchAPIGetAllRequest = async () => {
+        
+    }*/
+
+    useEffect(()=>{
+        getAllRequest(userLogged.token)
+        .then(res => setRequests(res))
+        .catch(err => {console.log(err)})
+    },[])
+
+    if(!requests) return <h1>Loading ...</h1>
+
+    const respondRequest = async (request,type) => {
+        let functionReference = type === "approve" ? approveRequest : rejectRequest;
+
+        let data = await functionReference(userLogged.token,request._id);
+        if(data.success){
+            let newRequests = requests.filter(req => req._id !== request._id)
+            setRequests(newRequests)
+        }
+
+    }
+
+    return(
+        <div className = "body">
+            {requests.map(request => {
+                {console.log(request)}
+                return (
+                    <div key = {request._id}  style={{width:"90vw",display:'flex',alignItems:'center',justifyContent:"space-around",marginTop:"12px",backgroundColor:"lightblue" }}> 
+                        <img  src = {request.logoStore.url} style={{width:"100px"}}/>
+                        <div style={{width:"60%",display:'flex',flexDirection:"column"}}>
+                            <div style= {{display:'flex',justifyContent:"space-around"}}>
+                                <span>From : {`${request.userOfRequest.firstName} ${request.userOfRequest.lastName}`}</span>
+                                <img  src = {request.userOfRequest.userImg.url} style={{width:"40px"}}/>
+                            </div>
+                            <div style={{display:'flex',justifyContent:"space-around"}}>
+                                <span>Category: {request.category.nameCategory}</span>
+                                <span>Descripcion:  {request.description}</span>
+                            </div>
+                        </div>    
+                        <div style = {{width:"10%",display:"flex", flexDirection:"column"}}>
+                            <button onClick = {()=> respondRequest(request,"approve")}>Approve</button>
+                            <button onClick = {()=> respondRequest(request,"reject")}>Reject</button>
+                        </div>
+                    </div>
+                )
+            })}
+            
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        userLogged : state.authReducer.userLogged,
+    }
+}
+
+const mapDispatchToProps = {
+    getAllRequest: adminAppActions.getAllRequest,
+    approveRequest: adminAppActions.approveRequest,
+    rejectRequest: adminAppActions.rejectRequest,
+}
+
+
+export default connect(mapStateToProps,mapDispatchToProps)(AdminApp)
--- frontend/src/pages/AdminGral.js
+++
@@ -1,6 +0,0 @@
-const AdminGral = () => {
-    return(
-        <h1>AdminGral</h1>
-    )
-}
-export default AdminGral
